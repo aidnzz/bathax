0150ac320a49d3b5e328a7d70dd952d672d2fd7a
4idn <67414260+4idn@users.noreply.github.com>
2021-03-03 19:45:18 +0000

    Update memory.hpp

--- bathax/memory.hpp
+++ bathax/memory.hpp
@@ -5,7 +5,7 @@
 
 namespace memory {
     template<typename T>
-    [[nodiscard]] auto read(const uintptr_t addr) -> std::add_lvalue_reference_t<T>
+    [[nodiscard]] inline auto read(const uintptr_t addr) -> std::add_lvalue_reference_t<T>
     {
         return *reinterpret_cast<std::add_pointer_t<T>>(addr);
     }
