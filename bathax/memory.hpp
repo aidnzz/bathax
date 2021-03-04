17d780ece5696d949479c31ccd01cbf9bb2de824
4idn <67414260+4idn@users.noreply.github.com>
2021-03-04 22:09:10 +0000

    Update memory.hpp

--- bathax/memory.hpp
+++ bathax/memory.hpp
@@ -3,7 +3,8 @@
 #include <cinttypes>
 #include <type_traits>
 
-namespace memory {
+namespace memory 
+{
     template<typename T>
     [[nodiscard]] inline auto read(const uintptr_t addr) -> std::add_lvalue_reference_t<T>
     {
